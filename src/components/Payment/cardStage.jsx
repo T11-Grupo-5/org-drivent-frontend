effb386f3541fc0fafc4a1b39349d153f859e428
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-10-18 21:35:53 -0300

    component cardZone

--- src/components/Payment/cardStage.jsx
+++ src/components/Payment/cardStage.jsx
@@ -1,16 +1,36 @@
 import { styled } from "styled-components";
-
-export function CardContainer (){
-    return(
+import { CardZone } from "./Cardzone";
+export function CardContainer() {
+    return (
         <CsCardContainer>
-
+            <div className="container">
+                <h5>Ingresso escolhido</h5>
+                <div className="content">
+                    <p className="negrito">Presencial + Com Hotel</p>
+                    <p>R$ 600</p>
+                </div>
+            </div>
+            <div className="container">
+                <h5>Pagamento</h5>
+                <CardZone/>
+            </div>
+            <button>FINALIZAR PAGAMENTO</button>
         </CsCardContainer>
     );
 }
 
-const CsCardContainer  = styled.div`
+const CsCardContainer = styled.div`
     border: 1px solid red;
     *{
         border: 1px solid red;
+    }
+    .content{
+        width: 290px;
+        height: 108px;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
 `;
