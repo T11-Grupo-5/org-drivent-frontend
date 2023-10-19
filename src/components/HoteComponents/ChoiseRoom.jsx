2c42e442312e203dca084b1225003487f9628e52
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-10-19 17:37:02 -0300

    choiceRoom

--- src/components/HoteComponents/ChoiseRoom.jsx
+++ src/components/HoteComponents/ChoiseRoom.jsx
@@ -1,14 +1,31 @@
 import { styled } from "styled-components";
+import RoomMold from "./RoomMold";
 
 export default function ChoiceRoom() {
   return (
     <CsChoiceRoom>
         <h1>Ótima pedida! Agora escolha seu quarto:</h1>
-        
+        <div className="rooms">
+          <RoomMold/>
+          <RoomMold/>
+          <RoomMold/>
+          <RoomMold/>
+          <RoomMold/>
+          <RoomMold/>
+          <RoomMold/>
+          <RoomMold/>
+          <RoomMold/>
+          <RoomMold/>
+          <RoomMold/>
+        </div>
     </CsChoiceRoom>
   );
 }
 
 const CsChoiceRoom = styled.div`
-
+  .rooms{
+    display: flex;
+    flex-wrap: wrap;
+    //justify-content: space-between;
+  }
 `;
