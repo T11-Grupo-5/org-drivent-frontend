e080468a03e0570be30a642d1831b242ee971e94
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-10-19 20:54:49 -0300

    MaybeChange

--- src/components/HoteComponents/MaybeChange.jsx
+++ src/components/HoteComponents/MaybeChange.jsx
@@ -1,13 +1,18 @@
 import { styled } from "styled-components";
-
+import HotelMold from "./HotelMold";
 export default function MaybeChange() {
   return (
     <CsMaybeChange>
-      <h1>Destinado a possivel mudança de quartos</h1>
+      <h1>Você já escolheu seu quarto:</h1>
+      <HotelMold/>
+      <button>TROCAR DE QUARTO</button>
     </CsMaybeChange>
   );
 }
 
 const CsMaybeChange = styled.div`
-
+  border: 1px solid;
+  *{
+  border: 1px solid;
+  }
 `;
