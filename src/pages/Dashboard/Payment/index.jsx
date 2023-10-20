f343005c1650662db143c80d5f61151ada0474bc
HenriqueSilva <HenriqueSilva254@gmail.com>
2023-10-20 15:06:39 -0300

    update css payments

--- src/pages/Dashboard/Payment/index.jsx
+++ src/pages/Dashboard/Payment/index.jsx
@@ -10,8 +10,9 @@ export default function Payment() {
   const { userData } = useContext(UserContext);
   const [tickets, setTickets] = useState([])
   const [ticketstype, setTicketType] = useState('')
-  const [selectTickets, setSelectTickets] = useState(false)
   const [hotelInclude, sethotelInclude] = useState('')
+  const [ticketHotel, setticketHotel] = useState('')
+  const [corSelect, setcorSelect] = useState('')
   
   useEffect(async()=>{
     try {
@@ -22,55 +23,76 @@ export default function Payment() {
     }
   }, [])
 
+
   function selectTicket(ticket){
     if(ticket === "presencial"){
+      setcorSelect("Presencial")
       setTicketType(
           <>
             <h5>Ótimo! Agora escolha sua modalidade de hospedagem</h5>
             <div className="options">
-              <div onClick={()=>{withHotel(0)}} className="inPerson option">Sem Hotel
+              <div onClick={()=>{withHotel(0)}} className={hotelInclude === "withoutHotel"? "inPerson option select":"inPerson option"}>Sem Hotel
               <p>+ R$ 0</p>
               </div>
-              <div onClick={()=>{withHotel(350)}} className="online option">Com Hotel
+              <div onClick={()=>{withHotel(350)}} className={hotelInclude === "withHotel"? "inPerson option select":"inPerson option"}>Com Hotel
               <p>+ R$ 350</p>
               </div>
             </div>
           </>
       )
     }else {
+      setcorSelect("Online")
       setTicketType(
       <div className="container">
         <h5>Fechado! O total ficou em <strong>R$ 100</strong>. Agora é só confirmar:</h5>
         <button onClick={()=>toReserve(100)}>RESERVAR INGRESSO</button>
       </div>
     )}
-    sethotelInclude('')
-    console.log(ticket)
+    setticketHotel('')
+   
+    console.log(corSelect)
   }
 
   function withHotel(money){
     let price = 0
-    if(money > 0){
+    let include = ''
+    if(money > 0){    
+      include = "withHotel"
       price = money + 250
-    }else{
+    }else{ 
+      include = "withoutHotel"
       price = 250
     }
-      
-      sethotelInclude(
+    setTicketType(
+      <>
+        <h5>Ótimo! Agora escolha sua modalidade de hospedagem</h5>
+        <div className="options">
+          <div onClick={()=>{withHotel(0)}} className={include === "withoutHotel"? "inPerson option select":"inPerson option"}>Sem Hotel
+          <p>+ R$ 0</p>
+          </div>
+          <div onClick={()=>{withHotel(350)}} className={include === "withHotel"? "inPerson option select":"inPerson option"}>Com Hotel
+          <p>+ R$ 350</p>
+          </div>
+        </div>
+      </>
+    )
+    setticketHotel(
       <div className="container">
         <h5>Fechado! O total ficou em R$ <strong>{money + 250}</strong>. Agora é só confirmar:</h5>
         <button onClick={()=>toReserve(price)}>RESERVAR INGRESSO</button>
       </div>
     )
+    
   }
   function toReserve(price){
-    console.log(tickets)
+    console.log(price)
     
     const ticket = tickets.filter((ticket) => {
       return ticket.price === price
     })
     console.log(ticket)
-    alert('ticket Reservado')
+    console.log(tickets)
+    //alert('ticket Reservado')
     }
 
   return (
@@ -80,10 +102,10 @@ export default function Payment() {
       <div className="container">
         <h5>Primeiro, escolha sua modalidade de ingresso</h5>
           <div className="options">
-          <div onClick={()=>{selectTicket("presencial")}} className="inPerson option">Presencial
+          <div onClick={()=>{selectTicket("presencial")}} className={corSelect === "Presencial"? "inPerson option select":"inPerson option"}>Presencial
             <p>R$ 250</p>
           </div>
-          <div onClick={()=>{selectTicket("online")}} className="inPerson option">Online
+          <div onClick={()=>{selectTicket("online")}} className={corSelect === "Online"? "inPerson option select":"inPerson option"}>Online
             <p>R$ 100</p>
           </div>
         </div>
@@ -91,7 +113,7 @@ export default function Payment() {
       <div className="container">
         {ticketstype}
       </div>
-        {hotelInclude}
+        {ticketHotel}
         <CardContainer/>
     </CsPayment>
   )
@@ -145,6 +167,10 @@ const CsPayment = styled.div`
       justify-content: center;
       border: 1px solid #CECECE;
       border-radius: 20px;
+     
+    }
+    .select{
+      background-color:#FFEED2;
     }
   }
   button{
