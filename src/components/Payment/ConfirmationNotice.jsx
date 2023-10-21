0e20e1298d5cfc06e69baeba37fa374b7a35a05e
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-10-21 15:41:58 -0300

    creditCard

--- src/components/Payment/ConfirmationNotice.jsx
+++ src/components/Payment/ConfirmationNotice.jsx
@@ -1,13 +1,40 @@
 import { styled } from "styled-components";
 
-export function ConfirmationNotice(){
-    return(
+export function ConfirmationNotice() {
+    return (
         <CsConfirmationNotice>
-            ESTA PARTE É SERVE PARA AVISAR QUE VC PAGOU COM SUCESSO.
+            <div className="green">
+                <ion-icon name="checkmark-circle-sharp"></ion-icon>
+            </div>
+            <div className="text">
+                <div className="bold">
+                    Pagamento confirmado!
+                </div>
+                <div className="normal">
+                    Prossiga para escolha de hospedagem e atividades
+                </div>
+            </div>
+
+
         </CsConfirmationNotice>
     )
 }
 
 const CsConfirmationNotice = styled.div`
-   
+    display: flex;
+    align-items: center;
+    //justify-content: center;
+   .green{
+    color: #36B853;
+    font-size: 40px;
+    display: flex;
+    align-items: center;
+   }
+   .bold{
+    font-weight: 700;
+   }
+   .normal{
+    font-weight: 400;
+
+   }
 `;
